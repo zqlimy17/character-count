5e3b029a8683078dcedf66e018b185b51d8985f8
zqlimy17 <zqlimy@gmail.com>
2020-05-23 21:20:04 +0800

    setup of core files

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import "./App.css";
+
+function App() {
+    return (
+        <Container className='App' maxWidth='xl'>
+            <h1>Character Count</h1>
+        </Container>
+    );
+}
+
+export default App;
