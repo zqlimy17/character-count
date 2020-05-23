7ea27473b1f16ccfc82cfbbdffa40efdbc781e8f
zqlimy17 <zqlimy@gmail.com>
2020-05-23 22:15:07 +0800

    added words hash

--- app/src/components/Count.tsx
+++ app/src/components/Count.tsx
@@ -6,17 +6,23 @@ const Count: React.FC<{ input: string }> = ({ input }) => {
     >(0);
     const [characterCount, setCharacterCount] = useState<number>(0);
     const [wordCount, setWordCount] = useState<number>(0);
+    const [wordsArray, setWordsArray] = useState<string[]>([]);
 
     useEffect(() => {
+        let wordHash: any = {};
         let wordLength = input
             .trim()
             .split(" ")
             .filter((word) => word.length > 0);
         let characterLength = input.replace(/\n|\ /g, "");
+        setWordsArray(wordLength);
+        for (let i = 0; i < wordsArray.length; i++) {
+            wordHash[wordsArray[i]] = (wordHash[wordsArray[i]] || 0) + 1;
+        }
         setWordCount(wordLength.length);
         setCharacterCount(characterLength.length);
         setCharacterWihtoutSpaces(input.length);
-        console.log(input);
+        console.log(wordHash);
     }, [input]);
 
     return (
